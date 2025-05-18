0f534c4ebc555a69eec07bec7594c0ab60e36f3f
David Van Kriedt <davidvankriedt@icloud.com>
2025-05-18 15:39:57 +1000

    Delete dist directory

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.DB_HOST = exports.DB_PASS = exports.DB_USER = exports.DB_NAME = void 0;
-exports.DB_NAME = process.env.DB_NAME;
-exports.DB_USER = process.env.DB_USER;
-exports.DB_PASS = process.env.DB_PASS;
-exports.DB_HOST = process.env.DB_HOST;
-exports.PORT = process.env.PORT;
